8fd7eeaa81045a16322047d2cb45963cf264165d
Dieter Stinglhamber <elhebert@hotmail.be>
2022-08-29 21:13:08 +0200

    update parsing

--- app/Console/Commands/RetrieveList.php
+++ app/Console/Commands/RetrieveList.php
@@ -32,18 +32,6 @@ class RetrieveList extends Command
      */
     public function handle()
     {
-        // $response = Http::accept('application/xml')
-        //     ->connectTimeout(60)
-        //     ->get('https://boardgamegeek.com/xmlapi/geeklist/301669', [
-        //         'comments' => 1,
-        //     ]);
-
-        // if (Str::of($response->body())->contains('Your request for this geeklist has been accepted and will be processed.')) {
-        //     return $this->handle(Carbon::now()->addSeconds(30));
-        // }
-
-        // Storage::put('list.xml', $response->body());
-
         RetrieveAdList::dispatch();
 
         return 0;
